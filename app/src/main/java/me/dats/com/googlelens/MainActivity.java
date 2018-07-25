86892877208396908cd3181b01adea938cd4d679
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2018-07-26 02:25:53 +0530

    Improve UI Now

--- app/src/main/java/me/dats/com/googlelens/MainActivity.java
+++ app/src/main/java/me/dats/com/googlelens/MainActivity.java
@@ -1,13 +1,74 @@
 package me.dats.com.googlelens;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
+import com.wonderkiln.camerakit.CameraKitEventCallback;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    CameraView cameraView;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraView = findViewById(R.id.cameraView);
+        floatingActionButton = findViewById(R.id.fab_take_photo);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.captureImage(new CameraKitEventCallback<CameraKitImage>() {
+                    @Override
+                    public void callback(CameraKitImage cameraKitImage) {
+                        getLabelsfromDevice(cameraKitImage.getBitmap());
+                    }
+                });
+            }
+        });
+    }
+
+    private void getLabelsfromDevice(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionLabelDetector detector = FirebaseVision.getInstance().getVisionLabelDetector();
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionLabel> firebaseVisionLabels) {
+
+                        for (FirebaseVisionLabel labels : firebaseVisionLabels){
+                            Log.d("TAG", "onSuccess: "+labels.getConfidence());
+                            Log.d("TAG", "onSuccess: "+labels.getLabel());
+                        }
+
+                    }
+
+                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
     }
 }
